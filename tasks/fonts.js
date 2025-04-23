1d96c55adef7bba3190e2be0d7b12a934584d059
David Lisitsyn <davidfox15@yandex.ru>
2025-04-23 22:40:25 +0300

    Update gulp fonts task

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -1,27 +1,23 @@
 const { src, dest } = require("gulp");
-// const ttf2woff2 = require("gulp-ttf2woff2");
-// const ttf2woff = requier("gulp-ttf2woff");
+const ttf2woff2 = require("gulp-ttf2woff2");
+const ttf2woff = require("gulp-ttf2woff");
 
 function ttf(done) {
-  src("src/fonts/**/*.ttf", { encoding: false, removeBOM: false }).pipe(
-    dest("public/fonts/"),
-  );
-  // src(["src/fonts/**/*.ttf"], {
-  //   encoding: false, // Important!
-  //   removeBOM: false,
-  // })
-  //   .pipe(ttf2woff2())
-  //   .pipe(dest("public/fonts/"));
-  //
-  // src(["src/fonts/**/*.ttf"], {
-  //   encoding: false, // Important!
-  //   removeBOM: false,
-  // })
-  //   .pipe(ttf2woff())
-  //   .pipe(dest("public/fonts/"));
+  src(["src/fonts/**/*.ttf"], {
+    encoding: false, // Important!
+    removeBOM: false,
+  })
+    .pipe(ttf2woff2())
+    .pipe(dest("public/fonts/"));
+
+  src(["src/fonts/**/*.ttf"], {
+    encoding: false, // Important!
+    removeBOM: false,
+  })
+    .pipe(ttf2woff())
+    .pipe(dest("public/fonts/"));
 
   done();
 }
 
 module.exports = ttf;
-// export default ttf;
