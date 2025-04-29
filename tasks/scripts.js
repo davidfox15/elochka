d0da58e3e0f914cbd0f7a13a228a92bb434e9179
David Lisitsyn <davidfox15@yandex.ru>
2025-04-29 13:23:05 +0300

    Updates, new pages

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -5,12 +5,14 @@ const rename = require("gulp-rename");
 const browserSync = require("browser-sync");
 
 function scripts() {
-  return src("src/scripts/*.js")
-    .pipe(concat("all.js")) // Объединение всех JS-файлов в один (all.js)
-    .pipe(uglify()) // Минимизация JS
-    .pipe(rename({ suffix: ".min" })) // Добавление .min к имени файла
-    .pipe(dest("public/scripts"))
-    .pipe(browserSync.stream());
+  return (
+    src("src/scripts/*.js")
+      // .pipe(concat("all.js")) // Объединение всех JS-файлов в один (all.js)
+      .pipe(uglify()) // Минимизация JS
+      .pipe(rename({ suffix: ".min" })) // Добавление .min к имени файла
+      .pipe(dest("public/scripts"))
+      .pipe(browserSync.stream())
+  );
 }
 
 module.exports = scripts;
